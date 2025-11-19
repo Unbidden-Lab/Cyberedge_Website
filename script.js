459d173c584ca9700d2aac1f39366acdd00c4f50
Arkin Jaromamay <arkinphoenix.jaromamay@tup.edu.ph>
2025-11-19 10:48:06 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("ctaBtn").addEventListener("click", () => {
+    const output = document.getElementById("outputMessage");
+    output.textContent = "Thank you for visiting Cyberedge!";
+});
